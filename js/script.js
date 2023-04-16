ef96f72323c51b6b28b14d4c541d0752b776dee8
Pedro Araujo <shikbane.developer@gmail.com>
2023-04-16 16:19:52 -0300

    Refatoração mais pequenos ajustes gráficos

--- js/script.js
+++ js/script.js
@@ -1,87 +1,40 @@
 "use strict";
 const title = document.querySelector(".title");
-const manufact = document.querySelector(".manufact");
 const modal = document.querySelector(".modal");
 const overlay = document.querySelector(".overlay");
-const btnOpenElysionChar = document.querySelector(".show-elysion-char");
-const elysionModal = document.querySelector(".elysion-modal");
 const btnCloseModal = document.querySelector(".close-modal");
-const btnRapi = document.querySelector(".rapi");
-const btnHelm = document.querySelector(".helm");
-const btnBrid = document.querySelector(".brid");
-const btnQuency = document.querySelector(".quency");
-const btnSin = document.querySelector(".sin");
-const btnCenti = document.querySelector(".centi");
-const btnAnis = document.querySelector(".anis");
-const btnSakura = document.querySelector(".sakura");
-const btnFolkwang = document.querySelector(".folkwang");
-
+const btnChar = document.querySelectorAll(".btn");
 const img = document.querySelector(".img");
 
-let char = "";
+let charName = "";
 
-const callChar = function (title) {
+//Declarando uma função de expressão que abri o modal + overlay + um determinado personagem
+const callChar = function (charName) {
   modal.classList.remove("hidden");
   overlay.classList.remove("hidden");
-  img.src = `./img/char1/${title.textContent}.gif`;
+  img.src = `./img/char1/${charName}.gif`;
 };
 
+//Função para fechar o modal e overlay
 const closeModal = function () {
   title.textContent = "Nikke";
-
   modal.classList.toggle("hidden");
   overlay.classList.toggle("hidden");
 };
 
-btnRapi.addEventListener("click", function () {
-  title.textContent = "Rapi";
-  callChar(title);
-});
-
-btnHelm.addEventListener("click", function () {
-  title.textContent = "Helm";
-  callChar(title);
-});
-
-btnBrid.addEventListener("click", function () {
-  title.textContent = "Brid";
-  callChar(title);
-});
-
-btnQuency.addEventListener("click", function () {
-  title.textContent = "Quency";
-  callChar(title);
-});
-
-btnSin.addEventListener("click", function () {
-  title.textContent = "Sin";
-  callChar(title);
-});
-
-btnCenti.addEventListener("click", function () {
-  title.textContent = "Centi";
-  callChar(title);
-});
-
-btnAnis.addEventListener("click", function () {
-  title.textContent = "Anis";
-  callChar(title);
-});
-
-btnSakura.addEventListener("click", function () {
-  title.textContent = "Sakura";
-  callChar(title);
-});
-
-btnFolkwang.addEventListener("click", function () {
-  title.textContent = "Folkwang";
-  callChar(title);
-});
+//Adicionando evento click em todos os botões
+for (let i = 0; i < btnChar.length; i++) {
+  btnChar[i].addEventListener("click", function () {
+    charName = btnChar[i].dataset.char;
+    title.textContent = charName;
+    callChar(charName);
+  });
+}
 
 btnCloseModal.addEventListener("click", closeModal);
 overlay.addEventListener("click", closeModal);
 
+//Função para fechar modal e overlay através da tecla ESC
 document.addEventListener("keydown", function (a) {
-  if (a.key === "Escape" && !modal.classList.contains("hidden")) 
-  closeModal();
+  if (a.key === "Escape" && !modal.classList.contains("hidden")) closeModal();
 });
